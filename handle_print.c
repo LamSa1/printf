7397a0e267766b6283ff0432f280318c05dd8281
asmaa.elfadil <asmaa.elfadil12@gmail.com>
2023-05-27 16:10:30 -0700

    printf

---
+++ handle_print.c
@@ -0,0 +1,49 @@
+#include "main.h"
+/**
+ * handle_print - Prints an argument based on its type
+ * @fmt: The formatted string in which the arguments are to be printed.
+ * @list: The list of arguments to be printed.
+ * @ind: Ind.
+ * @buffer: Buffer array used for handling the print.
+ * @flags: The flags used for calculating active flags.
+ * @width: The width specification for printing.
+ * @precision: The precision specification for printing.
+ * @size: The size specifier for printing.
+ * Return: 1 or 2
+ */
+int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
+	int flags, int width, int precision, int size)
+{
+	int i, unknow_len = 0, printed_chars = -1;
+	fmt_t fmt_types[] = {
+		{'c', print_char}, {'s', print_string}, {'%', print_percent},
+		{'i', print_int}, {'d', print_int}, {'b', print_binary},
+		{'u', print_unsigned}, {'o', print_octal}, {'x', print_hexadecimal},
+		{'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_non_printable},
+		{'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
+	};
+	for (i = 0; fmt_types[i].fmt != '\0'; i++)
+		if (fmt[*ind] == fmt_types[i].fmt)
+			return (fmt_types[i].fn(list, buffer, flags, width, precision, size));
+
+	if (fmt_types[i].fmt == '\0')
+	{
+		if (fmt[*ind] == '\0')
+			return (-1);
+		unknow_len += write(1, "%%", 1);
+		if (fmt[*ind - 1] == ' ')
+			unknow_len += write(1, " ", 1);
+		else if (width)
+		{
+			--(*ind);
+			while (fmt[*ind] != ' ' && fmt[*ind] != '%')
+			--(*ind);
+			if (fmt[*ind] == ' ')
+				--(*ind);
+			return (1);
+		}
+		unknow_len += write(1, &fmt[*ind], 1);
+		return (unknow_len);
+	}
+	return (printed_chars);
+}
